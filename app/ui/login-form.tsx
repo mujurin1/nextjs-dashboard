6a36165fbef64d807a012864026cded51174e05a
mujurin1 <mujurin25@yahoo.co.jp>
2025-01-01 13:10:42 +0900

    last-commit

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -23,6 +23,7 @@ export default function LoginForm() {
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
+
         <div className="w-full">
           <div>
             <label
@@ -38,6 +39,7 @@ export default function LoginForm() {
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
+                defaultValue="user@nextmail.com"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -57,6 +59,7 @@ export default function LoginForm() {
                 type="password"
                 name="password"
                 placeholder="Enter password"
+                defaultValue="123456"
                 required
                 minLength={6}
               />
